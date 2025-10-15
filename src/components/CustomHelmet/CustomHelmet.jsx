323c910efdc8980bf25d0dfe1cf1df5f2f858444
Sternee <sternee2332006@gmail.com>
2025-10-15 23:49:39 +0530

    Initial Commit

--- src/components/CustomHelmet/CustomHelmet.jsx
+++ src/components/CustomHelmet/CustomHelmet.jsx
@@ -4,7 +4,7 @@ import { Helmet } from "react-helmet-async";
 const CustomHelmet = ({ title }) => {
   return (
     <Helmet>
-      <title>{title} :: UB Jewellers</title>
+      <title>{title} :: Blooming Diamonds</title>
     </Helmet>
   );
 };
