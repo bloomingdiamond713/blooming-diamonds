a1bbb84cca4d88c9ef45fd50c363514f4b900280
Sternee <sternee2332006@gmail.com>
2025-10-19 15:53:28 +0530

    10X.1.4

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -11,7 +11,7 @@ const useProducts = () => {
     queryKey: ["products"],
     queryFn: async () => {
       const res = await axios.get(
-        `${import.meta.env.VITE_API_URL}products`
+        `${import.meta.env.VITE_API_URL}/api/products`
       );
       return res.data;
     },
